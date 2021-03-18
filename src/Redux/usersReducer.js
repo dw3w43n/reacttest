58aade27a703ce93618bbd647bbeb239af667c00
Narek <dw3w43n@gmail.com>
2021-03-18 12:55:15 +0400

    presentation and container components

--- src/Redux/usersReducer.js
+++ src/Redux/usersReducer.js
@@ -1,34 +1,14 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 
 let initialState = {
-  users: [
-    // {
-    //   id: 1,
-    //   photoUrl: "./images/profileimage.jpg",
-    //   followed: false,
-    //   fullName: "Arnaldo",
-    //   status: "programmer",
-    //   location: { city: "Violetborough", country: "Monaco" },
-    // },
-    // {
-    //   id: 2,
-    //   photoUrl: "./images/profileimage.jpg",
-    //   followed: true,
-    //   fullName: "Margarita",
-    //   status: "accounter",
-    //   location: { city: "South Kris", country: "Uruguay" },
-    // },
-    // {
-    //   id: 3,
-    //   photoUrl: "./images/profileimage.jpg",
-    //   followed: false,
-    //   fullName: "Gonzalo",
-    //   status: "designer",
-    //   location: { city: "Jacobiport", country: "Namibia" },
-    // },
-  ],
+  users: [],
+  pageSize: 20,
+  totalUsersCount: 0,
+  currentPage: 1,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -56,7 +36,19 @@ const usersReducer = (state = initialState, action) => {
     case SET_USERS: {
       return {
         ...state,
-        users: [...state.users, action.users],
+        users: [...action.users],
+      };
+    }
+    case SET_CURRENT_PAGE: {
+      return {
+        ...state,
+        currentPage: action.currentPage,
+      };
+    }
+    case SET_TOTAL_USERS_COUNT: {
+      return {
+        ...state,
+        totalUsersCount: action.count,
       };
     }
 
@@ -65,8 +57,16 @@ const usersReducer = (state = initialState, action) => {
   }
 };
 
-export const followAC = (userId) => ({ type: FOLLOW, userID: userId });
-export const unFollowAC = (userId) => ({ type: UNFOLLOW, userId: userId });
-export const setUsersAC = (users) => ({ type: SET_USERS, users: users });
+export const followAC = (userId) => ({ type: FOLLOW, userId });
+export const unFollowAC = (userId) => ({ type: UNFOLLOW, userId });
+export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({
+  type: SET_CURRENT_PAGE,
+  currentPage,
+});
+export const setUsersTotalCountAC = (totalUsersCount) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  count: totalUsersCount,
+});
 
 export default usersReducer;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,56 +1,67 @@
-import * as axios from "axios";
 import React from "react";
-import styles from "./users.module.css";
 import userPhoto from "../../assets/images/userimage.png";
+import styles from "./users.module.css";
 
-class UsersC extends React.Component {
-  constructor(props) {
-    super(props);
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        this.props.setUsers(response.data.items);
-      });
+const Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
   }
 
-  render() {
-    return (
+  return (
+    <div>
       <div>
-        {this.props.users.map((user) => {
+        {pages.map((p) => {
           return (
-            <div key={user.id}>
-              <span>
-                <div>
-                  <img className={styles.userPhoto} src={userPhoto} alt="" />
-                </div>
-                <div>
-                  {user.followed ? (
-                    <button onClick={() => this.props.unfollow(user.id)}>
-                      Unfollow
-                    </button>
-                  ) : (
-                    <button onClick={() => this.props.follow(user.id)}>
-                      Follow
-                    </button>
-                  )}
-                </div>
-              </span>
-              <span>
-                <span>
-                  <div>{user.name}</div>
-                  <div>{user.status}</div>
-                </span>
-                <span>
-                  <div>{"user.location.country"}</div>
-                  <div>{"user.location.city"}</div>
-                </span>
-              </span>
-            </div>
+            <span
+              onClick={(e) => props.onPageChanged(p)}
+              className={props.currentPage === p && styles.selectedPage}
+            >
+              {p}
+            </span>
           );
         })}
       </div>
-    );
-  }
-}
+      {props.users.map((user) => {
+        return (
+          <div key={user.id}>
+            <span>
+              <div>
+                <img
+                  className={styles.userPhoto}
+                  src={
+                    user.photos.small != null ? user.photos.small : userPhoto
+                  }
+                  alt=""
+                />
+              </div>
+              <div>
+                {user.followed ? (
+                  <button onClick={() => props.unfollow(user.id)}>
+                    Unfollow
+                  </button>
+                ) : (
+                  <button onClick={() => props.follow(user.id)}>Follow</button>
+                )}
+              </div>
+            </span>
+            <span>
+              <span>
+                <div>{user.name}</div>
+                <div>{user.status}</div>
+              </span>
+              <span>
+                <div>{"user.location.country"}</div>
+                <div>{"user.location.city"}</div>
+              </span>
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default UsersC;
+export default Users;
