df4359c5f59dec71dcd26cab78a86ec2393193ec
Narek <dw3w43n@gmail.com>
2021-03-28 14:53:56 +0400

    DAL, axios.create()

---
+++ src/api/api.js
@@ -0,0 +1,23 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  headers: {
+    "API-KEY": "0b18b1d8-3e9f-4296-aec5-d51c291492e2",
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => response.data);
+  },
+};
+
+export const getUsers2 = (currentPage = 1, pageSize = 10) => {
+  return instance
+    .get(`follow?page=${currentPage}&count=${pageSize}`)
+    .then((response) => response.data);
+};
